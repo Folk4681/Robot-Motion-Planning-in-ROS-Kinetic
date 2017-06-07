ae9441ca00e205e423a6ff54d7df89f4b79fe3ca
vsouda <vsouda@users.noreply.github.com>
2017-06-07 04:02:40 -0400

    Add files via upload

---
+++ global_planner.h
@@ -0,0 +1,34 @@
+ /** include the libraries you need in your planner here */
+ /** for global path planner interface */
+ #include <ros/ros.h>
+ #include <costmap_2d/costmap_2d_ros.h>
+ #include <costmap_2d/costmap_2d.h>
+ #include <nav_core/base_global_planner.h>
+ #include <geometry_msgs/PoseStamped.h>
+ #include <angles/angles.h>
+ #include <base_local_planner/world_model.h>
+ #include <base_local_planner/costmap_model.h>
+
+ using std::string;
+
+ #ifndef GLOBAL_PLANNER_CPP
+ #define GLOBAL_PLANNER_CPP
+
+ namespace global_planner {
+
+ class GlobalPlanner : public nav_core::BaseGlobalPlanner {
+ public:
+
+  GlobalPlanner();
+  GlobalPlanner(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
+
+  /** overridden classes from interface nav_core::BaseGlobalPlanner **/
+  void initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
+  bool makePlan(const geometry_msgs::PoseStamped& start,
+                const geometry_msgs::PoseStamped& goal,
+                std::vector<geometry_msgs::PoseStamped>& plan
+               );
+  };
+ };
+ #endif
+
